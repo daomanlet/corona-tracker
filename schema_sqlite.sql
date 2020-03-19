65541b21c56b18da062cfa480ae2a6cf9e777de2
xiazhenyu <xiazhenyu@weiresearch.com>
2020-03-20 04:02:48 +0800

    add insert sample

--- schema_sqlite.sql
+++ schema_sqlite.sql
@@ -1,14 +1,17 @@
-CREATE TABLE `daily_record` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `country_name` VARCHAR(64) NULL,
-  `cases` INT DEFAULT 0,
-  `new_cases` INT DEFAULT 0,
-  `deaths` INT DEFAULT 0,
-  `new_deaths` INT DEFAULT 0,
-  `date` DATE NOT NULL);
+CREATE TABLE daily_record (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  country_name VARCHAR(64) NULL,
+  cases INT DEFAULT 0,
+  new_cases INT DEFAULT 0,
+  deaths INT DEFAULT 0,
+  new_deaths INT DEFAULT 0,
+  date DATE NOT NULL);
 
   CREATE INDEX contry_name
   ON daily_record (country_name);
 
   CREATE INDEX record_date
   ON daily_record (date);
+
+insert into daily_record (country_name,cases, new_cases, deaths, new_deaths,date)
+  values ("china",1,2,3,4,"2020-01-01"),("usa",1,3,4,5,"2020-01-01");
